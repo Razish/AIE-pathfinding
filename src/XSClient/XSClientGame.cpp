1860b7eee7c6c65cdb9be2fd9cf02ed3c81c1159
Razish <mrrazish@gmail.com>
2015-02-09 18:05:13 +1100

    Rewrote tile structure to prepare for pathfinding. Added grid lines Fill 16:9 aspect ratio

--- src/XSClient/XSClientGame.cpp
+++ src/XSClient/XSClientGame.cpp
@@ -22,31 +22,59 @@ namespace XS {
 			AStar
 		};
 
-		enum TileType {
-			TILE_BLANK,
-			TILE_WALL,
-			TILE_START,
-			TILE_GOAL
+		enum class TileType {
+			Blank,
+			Wall,
+			Start,
+			Goal
 		};
 		
-		struct Node {
-			uint32_t x;
-			uint32_t y;
+		enum Direction {
+			NorthWest,
+			North,
+			NorthEast,
+			East,
+			SouthEast,
+			South,
+			SouthWest,
+			West,
+			NUM_DIRECTIONS
+		};
+		
+		struct Tile {
+			uint32_t	x, y;
+			// a cost of -1 means it is an unmoveable direction (i.e. off the map)
+			// the lower the cost, the better the move
+			int32_t		weights[NUM_DIRECTIONS];
+			TileType	type;
+
+#if 0
 			inline bool operator==( const Node &rhs ) {
 				if ( x == rhs.x && y == rhs.y ) {
 					return true;
 				}
 				return false;
 			}
+#endif
 		};
 
-		static const size_t dimensions = 16u;
+		static constexpr size_t dimensions[2] = { 32u, 18u };
 
 		static struct GameState {
-			uint8_t tiles[dimensions][dimensions];
-			std::list<Node> openList;
-			std::list<Node> closedList;
+			Tile tiles[dimensions[0]][dimensions[1]];
+
+			// tiles are added to the open list if it's to be explored
+			// when a tile is determined to be unsuitable, it's moved to the closed list and never checked again
+			std::list<Tile *> openList, closedList;
 		} state = {};
+		
+		static Tile *GetTile( const uint32_t x, const uint32_t y ) {
+			return &state.tiles[x][y];
+		}
+
+		static TileType GetTileType( const uint32_t x, const uint32_t y ) {
+			return state.tiles[x][y].type;
+		}
 
 		static Renderer::View *sceneView = nullptr;
 
@@ -55,165 +83,115 @@ namespace XS {
 		}
 
 		static void GenerateMaze( void ) {
-			// surround the map in a box
-			for ( size_t x = 0u; x < dimensions; x++ ) {
-				state.tiles[x][0] = TILE_WALL;
-				state.tiles[x][dimensions - 1] = TILE_WALL;
+			// initialise the tiles
+			for ( size_t x = 0u; x < dimensions[0]; x++ ) {
+				for ( size_t y = 0u; y < dimensions[1]; y++ ) {
+					state.tiles[x][y].x = x;
+					state.tiles[x][y].y = y;
+					state.tiles[x][y].type = TileType::Blank;
+				//	state.tiles[x][y].weights[North] = 0;
+				//	state.tiles[x][y].weights[East] = 0;
+				//	state.tiles[x][y].weights[South] = 0;
+				//	state.tiles[x][y].weights[West] = 0;
+				}
 			}
-			for ( size_t y = 0u; y < dimensions; y++ ) {
-				state.tiles[0][y] = TILE_WALL;
-				state.tiles[dimensions - 1][y] = TILE_WALL;
+
+			// surround the map in a box
+			for ( size_t x = 0u; x < dimensions[0]; x++ ) {
+				state.tiles[x][0].type = TileType::Wall;
+			//	state.tiles[x][0].weights[North] = -1; // unmoveable
+
+				state.tiles[x][dimensions[1] - 1].type = TileType::Wall;
+			//	state.tiles[x][dimensions[1] - 1].weights[South] = -1; // unmoveable
+			}
+			for ( size_t y = 0u; y < dimensions[1]; y++ ) {
+				state.tiles[0][y].type = TileType::Wall;
+			//	state.tiles[0][y].weights[East] = -1;
+
+				state.tiles[dimensions[0] - 1][y].type = TileType::Wall;
+			//	state.tiles[dimensions[0] - 1][y].weights[West] = -1;
 			}
 			
 			// randomly place the start position with 2 cell padding
 			int32_t startX = 0u;
 			int32_t startY = 0u;
 			while ( true ) {
-				startX = 1 + (rand() % (dimensions - 1));
-				startY = 1 + (rand() % (dimensions - 1));
-				if ( startX < 3 || startX >= dimensions - 3 ) {
+				startX = 1 + (rand() % (dimensions[0] - 1));
+				startY = 1 + (rand() % (dimensions[1] - 1));
+				if ( startX < 3 || startX >= dimensions[0] - 3 ) {
 					continue;
 				}
-				if ( startY < 3 || startY >= dimensions - 3 ) {
+				if ( startY < 3 || startY >= dimensions[1] - 3 ) {
 					continue;
 				}
-				if ( state.tiles[startX][startY] == TILE_BLANK ) {
+				if ( GetTileType( startX, startY ) == TileType::Blank ) {
 					break;
 				}
 			}
-			state.tiles[startX][startY] = TILE_START;
-			uint8_t *startNW	= &state.tiles[startX - 1][startY - 1];
-			uint8_t *startN		= &state.tiles[startX    ][startY - 1];
-			uint8_t *startNE	= &state.tiles[startX + 1][startY - 1];
-			uint8_t *startE		= &state.tiles[startX + 1][startY    ];
-			uint8_t *startSE	= &state.tiles[startX + 1][startY + 1];
-			uint8_t *startS		= &state.tiles[startX    ][startY + 1];
-			uint8_t *startSW	= &state.tiles[startX - 1][startY + 1];
-			uint8_t *startW		= &state.tiles[startX - 1][startY    ];
+			state.tiles[startX][startY].type = TileType::Start;
+			Tile *neighbours[NUM_DIRECTIONS] = {};
+			neighbours[NorthWest]	= GetTile( startX - 1, startY - 1 );
+			neighbours[North]		= GetTile( startX    , startY - 1 );
+			neighbours[NorthEast]	= GetTile( startX + 1, startY - 1 );
+			neighbours[East]		= GetTile( startX + 1, startY     );
+			neighbours[SouthEast]	= GetTile( startX + 1, startY + 1 );
+			neighbours[South]		= GetTile( startX    , startY + 1 );
+			neighbours[SouthWest]	= GetTile( startX - 1, startY + 1 );
+			neighbours[West]		= GetTile( startX - 1, startY     );
 
 			// randomly surround the start tile
 			int32_t side = rand() % 4;
-			if ( side == 0 ) {
-				// north
-				*startNW	= TILE_WALL;
-				*startN		= TILE_BLANK;
-				*startNE	= TILE_WALL;
-				*startE		= TILE_WALL;
-				*startSE	= TILE_WALL;
-				*startS		= TILE_WALL;
-				*startSW	= TILE_WALL;
-				*startW		= TILE_WALL;
-			}
-			else if ( side == 1 ) {
-				// east
-				*startNW	= TILE_WALL;
-				*startN		= TILE_WALL;
-				*startNE	= TILE_WALL;
-				*startE		= TILE_BLANK;
-				*startSE	= TILE_WALL;
-				*startS		= TILE_WALL;
-				*startSW	= TILE_WALL;
-				*startW		= TILE_WALL;
-			}
-			else if ( side == 2 ) {
-				// south
-				*startNW	= TILE_WALL;
-				*startN		= TILE_WALL;
-				*startNE	= TILE_WALL;
-				*startE		= TILE_WALL;
-				*startSE	= TILE_WALL;
-				*startS		= TILE_BLANK;
-				*startSW	= TILE_WALL;
-				*startW		= TILE_WALL;
-			}
-			else if ( side == 3 ) {
-				// west
-				*startNW	= TILE_WALL;
-				*startN		= TILE_WALL;
-				*startNE	= TILE_WALL;
-				*startE		= TILE_WALL;
-				*startSE	= TILE_WALL;
-				*startS		= TILE_WALL;
-				*startSW	= TILE_WALL;
-				*startW		= TILE_BLANK;
-			}
+			neighbours[NorthWest]->type	= TileType::Wall;
+			neighbours[North    ]->type	= (side == 0) ? TileType::Blank : TileType::Wall;
+			neighbours[NorthEast]->type	= TileType::Wall;
+			neighbours[East     ]->type	= (side == 1) ? TileType::Blank : TileType::Wall;
+			neighbours[SouthEast]->type	= TileType::Wall;
+			neighbours[South    ]->type	= (side == 2) ? TileType::Blank : TileType::Wall;
+			neighbours[SouthWest]->type	= TileType::Wall;
+			neighbours[West     ]->type	= (side == 3) ? TileType::Blank : TileType::Wall;
+
+
 
 			// randomly place the end position with 2 cell padding, at-least 3 cells away from the start
 			int32_t goalX = 0u;
 			int32_t goalY = 0u;
 			while ( true ) {
-				goalX = 1 + (rand() % (dimensions - 1));
-				goalY = 1 + (rand() % (dimensions - 1));
-				if ( goalX < 3 || goalX >= dimensions - 3 ) {
+				goalX = 1 + (rand() % (dimensions[0] - 1));
+				goalY = 1 + (rand() % (dimensions[1] - 1));
+				if ( goalX < 3 || goalX >= dimensions[0] - 3 ) {
 					continue;
 				}
-				if ( goalY < 3 || goalY >= dimensions - 3 ) {
+				if ( goalY < 3 || goalY >= dimensions[1] - 3 ) {
 					continue;
 				}
 				if ( std::abs( goalX - startX ) + std::abs( goalY - startY ) < 6 ) {
 					continue;
 				}
-				if ( state.tiles[goalX][goalY] == TILE_BLANK ) {
+				if ( GetTileType( goalX, goalY ) == TileType::Blank ) {
 					break;
 				}
 				
 			}
-			state.tiles[goalX][goalY] = TILE_GOAL;
-			uint8_t *goalNW	= &state.tiles[goalX - 1][goalY - 1];
-			uint8_t *goalN	= &state.tiles[goalX    ][goalY - 1];
-			uint8_t *goalNE	= &state.tiles[goalX + 1][goalY - 1];
-			uint8_t *goalE	= &state.tiles[goalX + 1][goalY    ];
-			uint8_t *goalSE	= &state.tiles[goalX + 1][goalY + 1];
-			uint8_t *goalS	= &state.tiles[goalX    ][goalY + 1];
-			uint8_t *goalSW	= &state.tiles[goalX - 1][goalY + 1];
-			uint8_t *goalW	= &state.tiles[goalX - 1][goalY    ];
+			state.tiles[goalX][goalY].type = TileType::Goal;
+			neighbours[NorthWest]	= GetTile( goalX - 1, goalY - 1 );
+			neighbours[North]		= GetTile( goalX    , goalY - 1 );
+			neighbours[NorthEast]	= GetTile( goalX + 1, goalY - 1 );
+			neighbours[East]		= GetTile( goalX + 1, goalY     );
+			neighbours[SouthEast]	= GetTile( goalX + 1, goalY + 1 );
+			neighbours[South]		= GetTile( goalX    , goalY + 1 );
+			neighbours[SouthWest]	= GetTile( goalX - 1, goalY + 1 );
+			neighbours[West]		= GetTile( goalX - 1, goalY     );
 
 			// randomly surround the goal tiles
 			side = rand() % 4;
-			if ( side == 0 ) {
-				// north
-				*goalNW	= TILE_WALL;
-				*goalN	= TILE_BLANK;
-				*goalNE	= TILE_WALL;
-				*goalE	= TILE_WALL;
-				*goalSE	= TILE_WALL;
-				*goalS	= TILE_WALL;
-				*goalSW	= TILE_WALL;
-				*goalW	= TILE_WALL;
-			}
-			else if ( side == 1 ) {
-				// east
-				*goalNW	= TILE_WALL;
-				*goalN	= TILE_WALL;
-				*goalNE	= TILE_WALL;
-				*goalE	= TILE_BLANK;
-				*goalSE	= TILE_WALL;
-				*goalS	= TILE_WALL;
-				*goalSW	= TILE_WALL;
-				*goalW	= TILE_WALL;
-			}
-			else if ( side == 2 ) {
-				// south
-				*goalNW	= TILE_WALL;
-				*goalN	= TILE_WALL;
-				*goalNE	= TILE_WALL;
-				*goalE	= TILE_WALL;
-				*goalSE	= TILE_WALL;
-				*goalS	= TILE_BLANK;
-				*goalSW	= TILE_WALL;
-				*goalW	= TILE_WALL;
-			}
-			else if ( side == 3 ) {
-				// west
-				*goalNW	= TILE_WALL;
-				*goalN	= TILE_WALL;
-				*goalNE	= TILE_WALL;
-				*goalE	= TILE_WALL;
-				*goalSE	= TILE_WALL;
-				*goalS	= TILE_WALL;
-				*goalSW	= TILE_WALL;
-				*goalW	= TILE_BLANK;
-			}
+			neighbours[NorthWest]->type	= TileType::Wall;
+			neighbours[North    ]->type	= (side == 0) ? TileType::Blank : TileType::Wall;
+			neighbours[NorthEast]->type	= TileType::Wall;
+			neighbours[East     ]->type	= (side == 1) ? TileType::Blank : TileType::Wall;
+			neighbours[SouthEast]->type	= TileType::Wall;
+			neighbours[South    ]->type	= (side == 2) ? TileType::Blank : TileType::Wall;
+			neighbours[SouthWest]->type	= TileType::Wall;
+			neighbours[West     ]->type	= (side == 3) ? TileType::Blank : TileType::Wall;
 
 			// partially surround the start/end positions
 			for ( size_t iteration = 0u; iteration < 5; iteration++ ) {
@@ -225,23 +203,23 @@ namespace XS {
 		}
 		
 		static void PrintMaze( void ) {
-			for ( size_t x = 0u; x < dimensions; x++ ) {
-				for ( size_t y = 0u; y < dimensions; y++ ) {
-					switch( state.tiles[x][y] ) {
+			for ( size_t x = 0u; x < dimensions[0]; x++ ) {
+				for ( size_t y = 0u; y < dimensions[1]; y++ ) {
+					switch( GetTileType( x, y ) ) {
 
-					case TILE_BLANK: {
+					case TileType::Blank: {
 						console.Print( "  " );
 					} break;
 
-					case TILE_WALL: {
+					case TileType::Wall: {
 						console.Print( "# " );
 					} break;
 
-					case TILE_START: {
+					case TileType::Start: {
 						console.Print( "S " );
 					} break;
 
-					case TILE_GOAL: {
+					case TileType::Goal: {
 						console.Print( "G " );
 					} break;
 
@@ -268,6 +246,7 @@ namespace XS {
 			static double lastTime = 0.0;
 			const double currentTime = Client::GetElapsedTime();
 			if ( lastTime < currentTime - 750 ) {
+				// step every 750ms
 				PathfindingStep( PathfindingAlgorithm::AStar );
 				lastTime = currentTime;
 			}
@@ -279,40 +258,37 @@ namespace XS {
 			const uint32_t screenWidth = Cvar::Get( "vid_width" )->GetInt();
 			const uint32_t screenHeight = Cvar::Get( "vid_height" )->GetInt();
 
-			const real32_t aspectCoefficient =
-				static_cast<real32_t>( screenHeight ) / static_cast<real32_t>( screenWidth );
+			const real32_t tileWidth = screenWidth / dimensions[0];
+			const real32_t tileHeight = screenHeight / dimensions[1];
 
-			const real32_t tileWidth = (screenWidth / dimensions) * aspectCoefficient;
-			const real32_t tileHeight = (screenHeight / dimensions);
-
-			for ( size_t x = 0u; x < dimensions; x++ ) {
-				for ( size_t y = 0u; y < dimensions; y++ ) {
+			for ( size_t x = 0u; x < dimensions[0]; x++ ) {
+				for ( size_t y = 0u; y < dimensions[1]; y++ ) {
 					const vector4 *colour = nullptr;
-					switch( state.tiles[x][y] ) {
+					switch( GetTileType( x, y ) ) {
 
-					case TILE_BLANK: {
+					case TileType::Blank: {
 						// ...
 					} break;
 
-					case TILE_WALL: {
+					case TileType::Wall: {
 						colour = &colourTable[ColourIndex( COLOUR_GREY )];
 					} break;
 
-					case TILE_START: {
+					case TileType::Start: {
 						colour = &colourTable[ColourIndex( COLOUR_GREEN )];
 					} break;
 
-					case TILE_GOAL: {
+					case TileType::Goal: {
 						colour = &colourTable[ColourIndex( COLOUR_RED )];
 					} break;
 
 					}
 
 					Renderer::DrawQuad(
-						x * tileWidth,
-						y * tileHeight,
-						tileWidth,
-						tileHeight,
+						(x * tileWidth) + 1.0f,
+						(y * tileHeight) + 1.0f,
+						tileWidth - 2.0f,
+						tileHeight - 2.0f,
 						0.0f, 0.0f, 1.0f, 1.0f, colour, nullptr );
 				}
 			}
